6abde2c7cf7cb21f11ae5bda28a005fd14402e3d
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-12-15 16:00:28 -0500

    Create fortune ring members

--- include/d/a/obj/d_a_obj_fortune_ring.h
+++ include/d/a/obj/d_a_obj_fortune_ring.h
@@ -4,6 +4,8 @@
 #include "d/a/obj/d_a_obj_base.h"
 #include "s/s_State.hpp"
 #include "s/s_StateMgr.hpp"
+#include "d/a/obj/d_a_obj_ring.h"
+#include "toBeSorted/d_emitter.h"
 
 class dAcOfortuneRing_c : public dAcObjBase_c {
 public:
@@ -13,8 +15,19 @@ public:
     STATE_FUNC_DECLARE(dAcOfortuneRing_c, Wait);
     STATE_FUNC_DECLARE(dAcOfortuneRing_c, Play);
 
-private:
-    /* 0x??? */ STATE_MGR_DECLARE(dAcOfortuneRing_c);
+    static dAcOfortuneRing_c* PTR;
+
+    /* 0x330 */ s32 mUnknownInt;
+    /* 0x334 */ dAcOring_c mRings[10];
+    /* 0x24a4 */ STATE_MGR_DECLARE(dAcOfortuneRing_c);
+    /* 0x24e0 */ dEmitter_c mEffects;
+    /* 0x2514 */ bool mShouldStart;
+    /* 0x2515 */ bool mUnknownFlag;
+    /* 0x2516 */ u8 padding_2516;
+    /* 0x2517 */ u8 mRingCount;
+    /* 0x2518 */ s32 mMultiplier;
+
+
 };
 
 #endif
